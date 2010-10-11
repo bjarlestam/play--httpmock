4eb31a8f0103e3ebd192d0b51697e4f27b15a1a0
Gaëtan RENAUDEAU <renaudeau.gaetan@gmail.com>
2010-10-11 18:09:07 +0200

    Continued http mock

--- httpmock/src/play/modules/httpmock/WSMock.java
+++ httpmock/src/play/modules/httpmock/WSMock.java
@@ -1,7 +1,65 @@
 package play.modules.httpmock;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import play.libs.WS.HttpResponse;
+import play.libs.WS.WSRequest;
 import play.libs.ws.WSAsync;
 
 public class WSMock extends WSAsync {
-    // TODO
+    
+    @Override
+    public WSRequest newRequest(String url) {
+        // TODO Auto-generated method stub
+        return new WSMockRequest(url);
+    }
+    
+    public class WSMockRequest extends WSAsyncRequest {
+        
+        public WSMockRequest(String url) {
+            super(url);
+        }
+        
+        @Override
+        public HttpResponse get() {
+            HttpResponse r = super.get();
+            return r;
+        }
+        
+        @Override
+        public Future<HttpResponse> getAsync() { // fake getAsync
+            return new Future<HttpResponse>() {
+                @Override
+                public boolean cancel(boolean mayInterruptIfRunning) {
+                    return false;
+                }
+
+                @Override
+                public HttpResponse get() throws InterruptedException, ExecutionException {
+                    return get();
+                }
+
+                @Override
+                public HttpResponse get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+                    return get();
+                }
+
+                @Override
+                public boolean isCancelled() {
+                    return false;
+                }
+
+                @Override
+                public boolean isDone() {
+                    return false;
+                }
+                
+            };
+        }
+        
+        
+    }
 }
